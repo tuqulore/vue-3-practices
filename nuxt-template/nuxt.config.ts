90a2adfb59e37a4d65996444376af9cb8d968e18
Kimiaki Kuno <knokmki612@gmail.com>
2022-01-14 13:57:27 +0900

    chore: re-setup CodeSandbox config

--- nuxt-template/nuxt.config.ts
+++ nuxt-template/nuxt.config.ts
@@ -2,5 +2,13 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+  ...(process.env.CODESANDBOX === "true" && {
+    vite: {
+      server: {
+        hmr: {
+          port: 443
+        }
+      }
+    }
+  })
 })
