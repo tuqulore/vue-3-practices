7277a38d8950d3c77957c16a0388d1af3fc23e1b
Kimiaki Kuno <knokmki612@gmail.com>
2022-01-20 16:58:59 +0900

    feat: 演習 reactive class binding event handling #19

---
+++ practice-reactive-class-binding-event-handling/src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import {ref} from "vue";
+export default {
+  setup() {
+    const isRed = ref(false);
+    const handleClick = () => {};
+    return {isRed, handleClick};
+  },
+};
+</script>
+
+<template>
+  <p>ボタンをクリックすると、線で囲われた領域が赤くなったり白くなったりするようにしてください</p>
+  <button @click="handleClick">ボタン</button>
+  <div class="colorful-box" />
+</template>
+
+<style>
+.colorful-box {
+  margin: 1rem 0;
+  width: 5rem;
+  height: 5rem;
+  border: 1px solid black;
+}
+
+.colorful-box.is-red {
+  background-color: red;
+}
+</style>
