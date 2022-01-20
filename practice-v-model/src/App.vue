10b3d2f2c8611b92a56180505add7b9705cc631b
Kimiaki Kuno <knokmki612@gmail.com>
2022-01-20 18:26:21 +0900

    feat: 演習 v-model #18

---
+++ practice-v-model/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import {ref} from "vue"
+export default {
+  setup() {
+    const input = ref("")
+    const handleInput = (event) => {
+      input.value = event.target.value
+    }
+    return {input, handleInput}
+  }
+}
+</script>
+
+<template>
+  <p>入力欄と表示欄の値が連動しています。この挙動を維持したまま、v-modelを使用するように書き換えてください。</p>
+  <input :value="input" @input="handleInput" />
+  <p>表示: {{input}}</p>
+</template>
+
+<style>
+</style>
