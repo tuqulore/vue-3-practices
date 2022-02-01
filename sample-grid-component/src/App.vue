f408ec7da7a162f7b4ae3e041a630566b3e0160a
Kimiaki Kuno <knokmki612@gmail.com>
2022-02-02 07:41:18 +0900

    feat: サンプル grid component

---
+++ sample-grid-component/src/App.vue
@@ -0,0 +1,41 @@
+<script>
+import {ref} from "vue"
+import DemoGrid from "./components/DemoGrid.vue"
+
+export default {
+  components: {
+    DemoGrid
+  },
+  setup() {
+    const searchQuery = ref('')
+    const gridColumns = ref(['name', 'power'])
+    const gridData = ref([
+      {name: 'Chuck Norris', power: Infinity},
+      {name: 'Bruce Lee', power: 9000},
+      {name: 'Jackie Chan', power: 7000},
+      {name: 'Jet Li', power: 8000}
+    ])
+    return {
+      searchQuery,
+      gridColumns,
+      gridData
+    }
+  }
+}
+</script>
+
+<template>
+  <form id="search">
+    Search
+    <input name="query" v-model="searchQuery" />
+  </form>
+  <DemoGrid :heroes="gridData" :columns="gridColumns" :filter-key="searchQuery" />
+</template>
+
+<style>
+body {
+  font-family: Helvetica Neue, Arial, sans-serif;
+  font-size: 14px;
+  color: #444;
+}
+</style>
