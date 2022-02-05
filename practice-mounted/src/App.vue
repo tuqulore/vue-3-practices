a357b7268816ec69d0bf47afb775debadc1d12c6
Kimiaki Kuno <knokmki612@gmail.com>
2022-02-05 20:25:37 +0900

    feat: 演習 mounted #17

---
+++ practice-mounted/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+// 何かをインポートする必要があります
+//import {} from "vue"
+export default {
+  setup() {
+    const showDialog = () => {alert("Hello, World!")}
+    return {showDialog}
+  }
+}
+</script>
+
+<template>
+  <p>ボタンを押すとダイアログが表示されます。コンポーネントがマウントされた際にダイアログが表示されるように処理を追加してください</p>
+  <button @click="showDialog()">ダイアログを表示</button>
+</template>
+
+<style>
+</style>
