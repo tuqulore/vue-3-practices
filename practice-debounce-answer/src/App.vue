1dd9fbbb85f547ed3ba402b551a8f6d43f591055
Kimiaki Kuno <knokmki612@gmail.com>
2022-02-05 18:58:44 +0900

    feat: 演習 debounce #15

---
+++ practice-debounce-answer/src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import {ref} from "vue"
+/**
+ * throttle-debounceパッケージの使い方はドキュメントを参照しましょう
+ * 参考: https://www.npmjs.com/package/throttle-debounce
+ */
+import {debounce} from "throttle-debounce"
+export default {
+  setup() {
+    const input = ref("")
+    const logs = ref([])
+    // debounceを使用することによって値を更新する処理の実行頻度を抑制することができます
+    const handleInput = debounce(1000, (event) => {
+      input.value = event.target.value
+      logs.value.push(event.target.value)
+    })
+    const handleClick = () => {
+      logs.value = []
+    }
+    return {input, logs, handleInput, handleClick}
+  }
+}
+</script>
+
+<template>
+  <p>入力履歴の1番目に「test」という文字が表示されるようにしてください</p>
+  <input :value="input" @input="handleInput" />
+  <p>入力内容: {{input}}</p>
+  <h4>入力履歴</h4>
+  <button @click="handleClick">リセット</button>
+  <ol>
+    <li value="0">ここに入力履歴が追加されます</li>
+    <li v-for="(log, index) in logs" :key="index">{{log}}</li>
+  </ol>
+</template>
+
+<style>
+</style>
