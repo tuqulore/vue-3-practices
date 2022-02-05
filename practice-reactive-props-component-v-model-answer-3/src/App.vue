0ba61162151d3bbeaf48f8bf09da50e51c7939dd
Kimiaki Kuno <knokmki612@gmail.com>
2022-02-05 18:20:26 +0900

    feat: 演習 reactive props component v-model #24

---
+++ practice-reactive-props-component-v-model-answer-3/src/App.vue
@@ -0,0 +1,43 @@
+<script>
+import {ref} from "vue"
+import ReverseButton from "./components/ReverseButton.vue"
+import MessageIndicator from "./components/MessageIndicator.vue"
+
+export default {
+  components: {
+    ReverseButton,
+    MessageIndicator
+  },
+  setup() {
+    const message = ref('Hello, World!')
+    return {
+      message,
+    }
+  }
+}
+</script>
+
+<template>
+  <p>同じコンポーネントが3つならび、文字の並びを反転するボタンを押すと一斉に文字の並びが反転します。</p>
+  <ol>
+    <li>ReverseButtonの呼び出しにv-modelディレクティブが使えるように変更してください</li>
+    <li>
+      Appに配置された文字の並びを反転するボタンを押しても
+      <code v-text="'MessageIndicator: {{message}}'" />
+      の表示が変わらないようにしてください
+    </li>
+    <li>それぞれのMessageIndicatorで文字の並びが反転できるように変更してください</li>
+  </ol>
+  <!--
+    v-modelディレクティブは引数をとることもできます。引数をとらない形に変更しても構いません。
+    参考: https://v3.ja.vuejs.org/guide/component-custom-events.html#v-model-%E3%81%AE%E5%BC%95%E6%95%B0
+  -->
+  <ReverseButton v-model:text="message" />
+  <p>App: {{message}}</p>
+  <MessageIndicator :message="message" />
+  <MessageIndicator :message="message" />
+  <MessageIndicator :message="message" />
+</template>
+
+<style>
+</style>
---
+++ practice-reactive-props-component-v-model/src/App.vue
@@ -0,0 +1,39 @@
+<script>
+import {ref} from "vue"
+import ReverseButton from "./components/ReverseButton.vue"
+import MessageIndicator from "./components/MessageIndicator.vue"
+
+export default {
+  components: {
+    ReverseButton,
+    MessageIndicator
+  },
+  setup() {
+    const message = ref('Hello, World!')
+    return {
+      message,
+    }
+  }
+}
+</script>
+
+<template>
+  <p>同じコンポーネントが3つならび、文字の並びを反転するボタンを押すと一斉に文字の並びが反転します。</p>
+  <ol>
+    <li>ReverseButtonの呼び出しにv-modelディレクティブが使えるように変更してください</li>
+    <li>
+      Appに配置された文字の並びを反転するボタンを押しても
+      <code v-text="'MessageIndicator: {{message}}'" />
+      の表示が変わらないようにしてください
+    </li>
+    <li>それぞれのMessageIndicatorで文字の並びが反転できるように変更してください</li>
+  </ol>
+  <ReverseButton :text="message" @update="message = $event" />
+  <p>App: {{message}}</p>
+  <MessageIndicator :message="message" />
+  <MessageIndicator :message="message" />
+  <MessageIndicator :message="message" />
+</template>
+
+<style>
+</style>
