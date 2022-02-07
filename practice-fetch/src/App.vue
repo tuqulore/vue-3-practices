b32b1c4070ee95c6786acca3c9693f1da31ea3d9
Kimiaki Kuno <knokmki612@gmail.com>
2022-02-07 10:42:23 +0900

    feat: 演習 fetch #28

---
+++ practice-fetch/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import {ref} from "vue"
+export default {
+  setup() {
+    const data = ref(null)
+    const load = async () => {
+      // この実行環境ではfetchがグローバルメソッドとして提供されているので、インポートすることなく利用することができます
+    }
+    load()
+    return {data}
+  }
+}
+</script>
+
+<template>
+  <a
+    href="https://portal.data.metro.tokyo.lg.jp/opendata-api/"
+    target="_blank"
+    rel="noopener noreferrer"
+  >東京都のオープンデータAPI</a>にあるいずれかのAPIエンドポイントからデータを取得し、あらかじめ用意されたdataの値に格納して表示されるようにしてください
+  <p>データ: {{data}}</p>
+</template>
+
+<style>
+</style>
