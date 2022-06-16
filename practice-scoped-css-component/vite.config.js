11b5d5c16eb05da3f05e4b1e00f58e31c5de0250
Kimiaki Kuno <knokmki612@gmail.com>
2022-06-16 09:09:11 +0900

    chore: drop codesandbox

--- practice-scoped-css-component/vite.config.js
+++ practice-scoped-css-component/vite.config.js
@@ -4,11 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  ...(process.env.CODESANDBOX === "true" && {
-    server: {
-      hmr: {
-        port: 443
-      }
-    }
-  })
 })
