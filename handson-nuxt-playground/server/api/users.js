ee28de9c0a38cefd5f2ddf42241088e624cbe159
Kimiaki Kuno <knokmki612@gmail.com>
2022-07-05 18:20:54 +0900

    feat: Nuxt 3 を触ってみようのハンズオン環境の整備 (#190)

--- handson-nuxt-playground/server/api/users.js
+++ handson-nuxt-playground/server/api/users.js
@@ -5,24 +5,32 @@ export default () => {
       email: "kitada_yuu@example.com",
       pref: "東京都",
       city: "府中市",
+      image:
+        "https://raw.githubusercontent.com/tuqulore/vue-3-practices/main/handson-nuxt-playground/public/user-photo-1.png",
     },
     {
       name: "小幡 有起哉",
       email: "obata_yukiya@example.com",
       pref: "大分県",
       city: "豊後高田市",
+      image:
+        "https://raw.githubusercontent.com/tuqulore/vue-3-practices/main/handson-nuxt-playground/public/user-photo-2.png",
     },
     {
       name: "伊藤 まひる",
       email: "itou_mahiru@example.com",
       pref: "熊本県",
       city: "葦北郡芦北町",
+      image:
+        "https://raw.githubusercontent.com/tuqulore/vue-3-practices/main/handson-nuxt-playground/public/user-photo-3.png",
     },
     {
       name: "有賀 雅功",
       email: "ariga_masatoshi@example.com",
       pref: "徳島県",
       city: "鳴門市",
+      image:
+        "https://raw.githubusercontent.com/tuqulore/vue-3-practices/main/handson-nuxt-playground/public/user-photo-4.png",
     },
   ];
 };
