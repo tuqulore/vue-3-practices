4b055a2a75991a4152af31c01531f3af9309f189
Kimiaki Kuno <knokmki612@gmail.com>
2023-12-05 05:24:17 +0900

    docs: スライド内コンポーネントのコードを<script>先頭にする

--- docs/components/TwoWayBindingSample.vue
+++ docs/components/TwoWayBindingSample.vue
@@ -1,12 +1,12 @@
+<script setup>
+import { ref } from "vue";
+
+const message = ref("Hello Vue!");
+</script>
+
 <template>
   <div id="two-way-binding">
     <p>{{ message }}</p>
     <input class="text-black p-1" v-model="message" />
   </div>
 </template>
-
-<script setup>
-import { ref } from "vue";
-
-const message = ref("Hello Vue!");
-</script>
