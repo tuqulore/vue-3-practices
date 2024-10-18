10523f4e91813d5145d74f972ef109e2ce0297fa
Hidetaro7 <Hidetaro7@users.noreply.github.com>
2024-10-18 09:54:04 +0000

    deploy: 0eac171cf927f4bbfc39c233ee5bef7cce9d40ea

---
+++ assets/slidev/default-BvYWcWKj.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-C7lKY8Q8.js";import{_ as r}from"../index-JwksoK2J.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
