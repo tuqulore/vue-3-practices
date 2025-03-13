ac309fe8040492ca08d9dd459c02dd26ea11d94d
knokmki612 <knokmki612@users.noreply.github.com>
2025-03-13 12:25:57 +0000

    deploy: 7af8307741faae8c1cede8c6aa7d3d0f436fddeb

---
+++ assets/slidev/default-BJKeEYNO.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-6zbViVe9.js";import{_ as r}from"../index-CEmGoRbI.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
