89402730fceb9168a438df6c4075ea90292fcd78
knokmki612 <knokmki612@users.noreply.github.com>
2026-02-02 23:54:35 +0000

    deploy: b436616980c98730ebfe213793708301bde78357

---
+++ assets/slidev/default-G-UyLdH3.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-D6EfhC-F.js";import{_ as r}from"../index-CfqAS0qD.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
